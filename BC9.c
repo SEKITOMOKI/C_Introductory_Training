0ac1f3b8bbaea12eac9007d94236d3e548ed5eac
SEKITOMOKI <SEKITOMOKI@163.com>
2023-05-26 20:37:46 +0800

    输出printf的返回值

--- BC9.c
+++ BC9.c
@@ -4,9 +4,9 @@
 int main()
 {
 	//输出Hello world!并把printf("Hello world!")的返回值赋值给a
-    //printf函数的返回值是输出的字符的个数
+	//printf函数的返回值是输出的字符的个数
 	int a = printf("Hello world!");
-	
+
 	//输出printf("Hello world!")的返回值，因为有12个字符，所以输出12
 	printf("\n%d", a);
 	return 0;
